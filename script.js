f47eed1713b3a65a3dfc52741496a3dfed6f94b2
ZelenskaAnna <anetty.mlt@gmail.com>
2023-11-21 17:24:32 +0100

    commit

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function navToggle(){
 	document.getElementById('headerNav').classList.toggle('is-active');
 }
+document.addEventListener("DOMContentLoaded", function () {
+	function navToggle() {
+	  var headerNav = document.getElementById('headerNav');
+	  headerNav.classList.toggle('is-active');
+	}
+  
+	var menuLinks = document.querySelectorAll('.header__a');
+  
+	menuLinks.forEach(function (link) {
+	  link.addEventListener('click', function () {
+		var headerNav = document.getElementById('headerNav');
+		headerNav.classList.remove('is-active');
+	  });
+	});
+  });
